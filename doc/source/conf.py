6d46456e4d04e35284f7a152eaf91008ec8d4d56
JaumeFigueras <jaume.figueras@upc.edu>
2025-08-17 00:40:43 +0200

    issue in gh_pages.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,6 +6,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join('..', 'test')))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
